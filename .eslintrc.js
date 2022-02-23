057d7653764ca1980df32f204ad4a39f6ae4fcb3
ovaphlow <ovaphlow@live.com>
2022-02-23 15:42:53 +0800

    update libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,13 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 13,
-  },
-  rules: {
-    'implicit-arrow-linebreak': 0,
-    'import/prefer-default-export': 0,
-    'import/no-named-default': 0,
-    'import/extensions': 0,
-  },
-};
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
